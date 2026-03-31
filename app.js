7bfe15dc430117f91d8da2c0c62a90d198d6a664
PRIYAM NILESH BHATT <priyambhatt013@gmail.com>
2026-03-31 12:42:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+let gameSequence=[];
+let userSequence=[];
+let started=false;
+let level=0;
+let h2=document.querySelector('h2');
+let btns=["apple","grapes","banana","orange"];
+let counter=-1;
+let acceptingInput=false;
+let popSound=new Audio("pop.mp3");
+let winSound=new Audio("win.mp3");
+let wrongSound=new Audio("wrong.mp3");
+function playPop(){
+    popSound.currentTime=0;
+    popSound.play();
+}
+function playWin(){
+    winSound.currentTime=0;
+    winSound.play();
+}
+function playWrong(){
+    wrongSound.currentTime=0;
+    wrongSound.play();
+}
+document.addEventListener("keypress", function(){
+    if(started===false){
+         console.log("game started!");
+         started=true;
+         levelUp();
+    }
+   
+});
+function gameFlash(btn){
+    playPop();
+    btn.classList.add("flash");
+    setTimeout(()=>{
+        btn.classList.remove("flash");
+    },250);
+}
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(()=>{
+        btn.classList.remove("userflash");
+    },250);
+}
+function levelUp(){
+    acceptingInput=false;
+    level++;
+    h2.innerText=`Level ${level}`;
+    let randidx= random();
+    let fruit=btns[randidx];
+    let randbtn=document.querySelector(`#${fruit}`);
+    gameSequence.push(randbtn);
+    //console.log(randidx);
+    gameFlash(randbtn);
+    setTimeout(()=>{
+        acceptingInput=true;
+    },300);
+
+}
+function gameEnd(){
+    playWrong();
+    h2.innerText=`Game over! Your Score is ${level-1}\nPress any key to start the game again.`;
+    document.querySelector("body").style.backgroundColor="red";
+    setTimeout(()=>{
+        document.querySelector("body").style.backgroundColor="white";
+    },1000
+    );
+    started=false;
+    gameSequence=[];
+    userSequence=[];
+    level=0;
+    counter=-1;
+    acceptingInput=false;
+}
+function btnPress(){
+    if(!started || !acceptingInput){
+        return;
+    }
+    if(started==true){
+        counter++;
+    let btn=this;
+    userFlash(btn);
+    if(gameSequence[counter]===btn){
+        playWin();
+        userSequence.push(btn);
+        if(userSequence.length==gameSequence.length){
+            acceptingInput=false;
+            counter=-1;
+            userSequence=[];
+            setTimeout(levelUp,1000);
+        }
+        
+    }    
+    else{
+        
+        gameEnd();
+    }
+    }
+    
+    
+}
+let allBtns=document.querySelectorAll(".btn");
+
+    for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+
+}
+
+function random(){
+    return Math.floor(Math.random()*4);
+}
